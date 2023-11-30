dcd5b02c2ec27eaf58ae195855a27bfb5c4981e0
Udit <royalboy0012@gmail.com>
2023-11-30 12:39:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ function App() {
         <Navbar />
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route path='/newTask' element={<NewTask />} />
         </Routes>
       </div>
     </BrowserRouter>
